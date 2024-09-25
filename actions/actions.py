10e98d63bd2d852d8f6e31323b39f46968198adc
shumExarta <shahum@exarta.com>
2024-09-25 17:36:23 +0500

    latest model trained

--- actions/actions.py
+++ actions/actions.py
@@ -1,19 +1,22 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
 from .functions import (
     get_product,
     get_product_position,
     createJson,
     get_data_from_api,
+    get_player_position,
     send_data_to_rag,
-    state_maintain
+    handle_conversation,
+    state_maintain,
 )
 from thefuzz import fuzz, process
 import json
 import math
 
-# json_file = "./products_list.json"
+# product_list = "./products.json"
 state = {}
 
 
@@ -30,7 +33,10 @@ class ActionMoveTo(Action):
 
         print("-----MOVE ACTION-----")
         # TRIGGING THE API ENDPOINT TO GET THE DATA HERE
-        product_list, product_names = get_data_from_api()
+        product_list, product_names, bot_id = get_data_from_api()
+        # bot_id = get_bot_id(product_list)
+        # player_position = get_player_position()
+        # print(f"PLAYER POSITION: {player_position}")
         
         slots = tracker.current_slot_values()
         bot_position = [64.342996, 825.109437, 90.150096]
@@ -48,7 +54,7 @@ class ActionMoveTo(Action):
             price=product['price']
         )
         
-        print(f"STATE : {state}")
+        # print(f"STATE : {state}")
         
         message = f"Sure! Let me bring out the {product['name']} for you"
 
@@ -56,16 +62,57 @@ class ActionMoveTo(Action):
             send_json = createJson(
                 iD=product["iD"],
                 name=product["name"],
-                desc=product["desc"],
+                desc=message,
                 translation=product["itemTransform"]["translation"],
                 action="PickItem",
-                additionalInformation=message
+                additionalInformation=message,
+                bot_id=bot_id
             )
 
         print(send_json)
         dispatcher.utter_message(message)
         dispatcher.utter_message(json_message=send_json)
+        
+        player_position = get_player_position()
+        print(f"PLAYER POSITION: {player_position}")
 
+        return []
+
+
+class ActionPlaceItem(Action):
+    def name(self) -> Text:
+        return "action_place_item"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        print("-----PLACE ITEM ACTION-----")
+        slots = tracker.current_slot_values()
+        
+        # TRIGGING THE API ENDPOINT TO GET THE DATA HERE
+        product_list, product_names, bot_id = get_data_from_api()
+        
+        product_slot = process.extractOne(slots['product'], product_names, scorer=fuzz.ratio, score_cutoff=50)
+        product = get_product(product_slot[0], product_list)
+        print(f"PRODUCT : {product}")
+        message = "No worries! Let me know how can I help you better?"
+        
+        send_json = createJson(
+                iD=product["iD"],
+                name=product["name"],
+                desc=message,
+                action="PlaceItem",
+                translation=product["itemTransform"]["translation"],
+                additionalInformation=message,
+                bot_id=bot_id
+            )
+
+        dispatcher.utter_message(message)
+        dispatcher.utter_message(json_message=send_json)
         return []
 
 
@@ -166,7 +213,8 @@ class ActionRag(Action):
 
         print("-----RAG ACTION-----")
 
-        intent = tracker.latest_message["text"]
+        intent = tracker.latest_message['text']
+        print(f"INTENT : {intent}")
         rag_response = send_data_to_rag(intent)
         
         send_json = createJson(
@@ -178,3 +226,33 @@ class ActionRag(Action):
         dispatcher.utter_message(json_message=send_json)
 
         return []
+
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        print("-----FALLBACK ACTION-----")
+
+        intent = tracker.latest_message['text']
+        print(f"INTENT : {intent}")
+        
+        
+        lang_graph_response = handle_conversation(intent)
+        
+        send_json = createJson(
+            action='Communicate',
+            additionalInformation=lang_graph_response
+        )
+        
+        dispatcher.utter_message(lang_graph_response)
+        dispatcher.utter_message(json_message=send_json)
+
+        return [UserUtteranceReverted()]
--- actions/functions.py
+++ actions/functions.py
@@ -7,15 +7,30 @@ with open('./products_list.json', 'r') as file:
     original_json_file = json.load(file)
 
 # API INTEGRATIONS FOR GETTING PRODUCT DATA
+# def get_data_from_api_test():
+#     url = "http://172.16.15.217:8000/product_list"
+#     response = requests.get(url=url)
+#     print(f"RESPONSE : {response}")
+
+# HANDLE OUT OF CONTEXT NLU BY USING LANGGRAPH
+def handle_conversation(text):
+    url = "http://127.0.0.1:8001/handle_conversation"
+    data = {'text': text}
+    response = requests.post(url=url, json=data['text'])
+    
+    return response.json()
+
+
+# GETTING THE DATA FROM THE API, PRODUCT DATA FROM THE 3D SCENE
 def get_data_from_api():
-    url = "http://172.16.15.217:8000/api/product_list"
+    url = "http://172.16.15.217:8000/product_list"
     response = requests.get(url=url)
     data = response.json()
     product_list = data['product_list']
     
     if (product_list != original_json_file):
         print("Data is changed. Updating JSON file.")
-        with open('./products.json', 'w') as file:
+        with open('./products_list.json', 'w') as file:
             json.dump(product_list, file)
     else:
         print('Data is not changed. No need to update the JSON file')
@@ -24,22 +39,34 @@ def get_data_from_api():
         json.dump(product_list, file, indent=4)
         
     product_names = [item['name'] for item in product_list]
+    bot_id = product_list[0]['iD']
 
-    return product_list, product_names
+    return product_list, product_names, bot_id
+
 
 # SENDING DATA TO RAG API AND GETTING RESPONSE FROM THERE
 def send_data_to_rag(text):
-    url = "http://172.16.15.217:8000/api/rag"
+    url = "http://172.16.15.217:8000/rag"
     data = {'text': text}
     response = requests.post(url=url, json=data['text'])
         
     return response.json()
+
+
+# GETTING THE POSITION OF THE PLAYER
+def get_player_position():
+    url = "http://172.16.15.217:8000/player_position"
+    response = requests.get(url=url)
+    data = response.json()
+    return data['player_position']
+
 
 # GETTING PRODUCT NAME FROM THE PRODUCT LIST
 def get_product(productName, productList):
     for product in productList:
         if product['name'] == productName :
             return product
+
 
 # GETTING PRODUCTS POSITION FROM THE LIST
 def get_product_position(productName, productList):
@@ -50,6 +77,7 @@ def get_product_position(productName, productList):
             z = product["itemTransform"]["translation"]["z"]
 
     return x, y, z
+
 
 # CREATING JSON TO SEND TO UNREAL
 def createJson(
@@ -62,6 +90,7 @@ def createJson(
     scale3D=None,
     action=None,
     additionalInformation=None,
+    bot_id=None,
     **kwargs,
 ):
     dict_json = {
@@ -75,7 +104,8 @@ def createJson(
             'scale3D': scale3D,
         },
         'action': action,
-        'additionalInformation': additionalInformation
+        'additionalInformation': additionalInformation,
+        'bot_id': bot_id,
     }
     # def format_value(value):
     #     if value is None:
@@ -101,16 +131,19 @@ def createJson(
 
     return dict_json
 
+
 # FUNCTION FOR MAINTAING THE STATES
 def state_maintain(
-    iD=None,
+    player_id=None,
+    id=None,
     name=None,
     desc=None,
     price=None,
     **kwargs,):
     
     state_dict = {
-        'iD': iD,
+        'player_id': player_id,
+        'id': id,
         'name': name,
         'desc': desc,
         'price': price,
